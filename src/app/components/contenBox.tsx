003e9f15c22aa817bdceb77fe7cfd0cbd8af8cf5
choidy180 <choidy180@naver.com>
2025-01-13 03:29:13 +0900

    'deploy'

--- src/app/components/contenBox.tsx
+++ src/app/components/contenBox.tsx
@@ -13,7 +13,8 @@ interface prop {
         tag: string;
         thumbnail: string;
         title: string;
-    }
+    },
+    ui: boolean;
 }
 
 const ContentBox = (prop:prop) => {
@@ -54,62 +55,67 @@ const ContentBox = (prop:prop) => {
         document.body.removeChild(link); // 링크 제거
     }
     return (
-        <div className="w-[324px] flex flex-col justify-start items-start p-[8px] border-[2px] border-solid border-[#7bed9f] transition-all bg-white rounded-[8px] hover:bg-gray-100">
+        <div className={`w-[324px] flex flex-col justify-start items-start p-[8px] border-[2px] border-solid border-[#7bed9f] transition-all bg-white rounded-[8px] hover:bg-gray-100 ${prop.ui === false && 'w-full'}`}>
             <audio className="hidden" ref={audioRef} controls>
                 <source src={`audio/${prop.content['folder']}/${prop.content['file_name']}`} type="audio/mp3"/>
             </audio>
-            <div className="w-full h-[170px] bg-[#e9e9e9] rounded-[6px] overflow-hidden group">
-                <Image
-                    src={`${prop.content.thumbnail}`}
-                    width={320}
-                    height={200}
-                    alt="..."
-                    className="object-cover"
-                />
-            </div>
-            <div className="w-full flex justify-start items-center">
-                {
-                    playing === false &&
-                    (
-                        <div onClick={()=> setPlaying(true)} className="mt-[8px] w-[38px] h-[38px] bg-[#f53b57] flex justify-center items-center rounded-full mr-[8px] cursor-pointer">
-                            <TbPlayerPlayFilled className="text-white text-[26px]"/>
-                        </div>
-                    )
-                }
-                {
-                    playing === true &&
-                    (
-                        <div onClick={()=> setPlaying(false)} className="mt-[8px] w-[38px] h-[38px] bg-[#3498db] flex justify-center items-center rounded-full mr-[8px] cursor-pointer">
-                            <IoPauseSharp className="text-white text-[26px]"/>
-                        </div>
-                    )
-                }
-                <p className="mt-[10px] w-[calc(100%-70px)] font-medium text-[18px] leading-[20px]">{prop.content.title}</p>
-            </div>
-            <div className="w-full min-h-[64px] flex flex-wrap justify-start items-start mt-[10px] gap-[8px]">
-                {
-                    prop.content.tag && prop.content.tag.split(',').map((content:string, index:number) => (
-                        <span 
-                            className={`
-                                text-white text-[16px] rounded-[8px] px-[10px] py-[2px]
-                                bg-[#2ecc71]
-                            `} 
-                            key={`tag-${index}`}
-                        >
-                            {content}
-                        </span>
-                    ))
-                }
-            </div>
-            <div className="w-full flex flex-col justify-start items-center mt-[20px]">
-                <p className="text-[16px] font-semibold flex justify-start items-center">Comment.. <IoChatbubbleEllipses className="w-[18px] h-[18px] ml-[6px] text-[#3498db]"/></p>
-                <p className="text-[18px] leading-[20px] mt-[8px] min-h-[100px]">{prop.content.comment}</p>
+            <div className={`w-full flex flex-col justify-start items-start ${prop.ui === false && '!flex-row'}`}>
+                <div className={`w-full h-[170px] bg-[#e9e9e9] rounded-[6px] overflow-hidden group ${prop.ui === false && '!w-[300px] h-[158px]'}`}>
+                    <Image
+                        src={`${prop.content.thumbnail}`}
+                        width={320}
+                        height={200}
+                        alt="..."
+                        className="object-cover"
+                    />
+                </div>
+                <div className={`w-full flex flex-col justify-start items-start ${prop.ui === false && 'w-[calc(100%-250px)] ml-[14px]'}`}>
+                    <div className={`w-full flex justify-start items-center ${prop.ui === false && 'w-[calc(100%-200px)]'}`}>
+                        {
+                            playing === false &&
+                            (
+                                <div onClick={()=> setPlaying(true)} className="mt-[8px] w-[38px] h-[38px] bg-[#f53b57] flex justify-center items-center rounded-full mr-[8px] cursor-pointer">
+                                    <TbPlayerPlayFilled className="text-white text-[26px]"/>
+                                </div>
+                            )
+                        }
+                        {
+                            playing === true &&
+                            (
+                                <div onClick={()=> setPlaying(false)} className="mt-[8px] w-[38px] h-[38px] bg-[#3498db] flex justify-center items-center rounded-full mr-[8px] cursor-pointer">
+                                    <IoPauseSharp className="text-white text-[26px]"/>
+                                </div>
+                            )
+                        }
+                        <p className="mt-[10px] w-[calc(100%-70px)] font-medium text-[18px] leading-[20px]">{prop.content.title}</p>
+                    </div>
+                    <div className={`w-full min-h-[64px] flex flex-wrap justify-start items-start mt-[10px] gap-[8px] ${prop.ui === false && '!min-h-[30px]'}`}>
+                        {
+                            prop.content.tag && prop.content.tag.split(',').map((content:string, index:number) => (
+                                <span 
+                                    className={`
+                                        text-white text-[16px] rounded-[8px] px-[10px] py-[2px]
+                                        bg-[#2ecc71]
+                                    `} 
+                                    key={`tag-${index}`}
+                                >
+                                    {content}
+                                </span>
+                            ))
+                        }
+                    </div>
+                    <div className={`w-full flex flex-col justify-start items-center mt-[20px] ${prop.ui === false && 'mt-[4px] !items-start'}`}>
+                        <p className="text-[16px] font-semibold flex justify-start items-center">Comment.. <IoChatbubbleEllipses className="w-[18px] h-[18px] ml-[6px] text-[#3498db]"/></p>
+                        <p className={`text-[18px] leading-[20px] mt-[8px] min-h-[100px] ${prop.ui === false && '!mt-[4px] !min-h-0'}`}>{prop.content.comment}</p>
+                    </div>
+                </div>
             </div>
             <button 
-                className="
+                className={`
                     w-full p-[4px] text-[18px] border-[2px] border-solid border-green-400 
                     rounded-[8px] mt-[20px] bg-white hover:bg-green-300 font-semibold
-                "
+                    ${prop.ui === false && 'absolute right-[8px] top-[-12px] w-[100px]'}
+                `}
                 onClick={downloadAudio}
             >
                 다운로드
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import ContentBox from "./components/contenBox";
 import LoadData from "../app/json/main_data.json";
+import { PiCardsBold, PiListFill } from "react-icons/pi";
 
 interface prop {
     comment: string;
@@ -18,6 +19,7 @@ interface prop {
 export default function Home() {
     const [theme, setTheme] = useState<number>(0);
     const [keyword, setKeyword] = useState<string>('');
+    const [ui, setUi] = useState<boolean>(true);
     return (
         <div className="w-full flex flex-col justify-start items-center p-[20px]">
             <h4 className="mt-[100px] text-[34px] font-bold en mb-[80px]">Copyright-free BGM</h4>
@@ -33,16 +35,17 @@ export default function Home() {
                 value={keyword}
                 onChange={(event)=> setKeyword(event?.target.value)}
             />
-            <div className="mt-[20px] flex justify-center items-center w-[calc(100%-20px)] max-w-[1000px] h-[36px] text-[24px] font-semibold">
+            <div className="mt-[20px] p-[8px] flex justify-center items-center w-[calc(100%-20px)] rounded-[8px] max-w-[1000px] h-[58px] text-[24px] font-semibold border-[3px] border-solid border-[#7bed9f]">
                 <div 
                     className={`
-                        bg-[#7bed9f] w-[33.3333%] h-[36px] rounded-[4px] 
-                        absolute transition-all left-0 translate-x-[100%] 
-                        ${theme === 0 && '!translate-x-0'}
-                        ${theme === 1 && '!translate-x-[100%]'}
-                        ${theme === 2 && '!translate-x-[200%]'}
-                    `}/>
-                <div className="w-full flex justify-center items-center cursor-pointer" onClick={()=> setTheme(0)} onMouseOver={()=> console.log(LoadData.data)}>
+                        bg-[#7bed9f] w-[calc(33.3333%-4px)] !h-[40px] rounded-[8px]
+                        absolute transition-all left-0 translate-x-[6px]
+                        ${theme === 0 && '!translate-x-[8px]'}
+                        ${theme === 1 && '!translate-x-[calc(100%+6px)]'}
+                        ${theme === 2 && '!translate-x-[calc(200%+4px)]'}
+                    `}
+                />
+                <div className="w-full flex justify-center items-center cursor-pointer" onClick={()=> setTheme(0)}>
                     <span>게임별</span>
                 </div>
                 <div className="w-full flex justify-center items-center cursor-pointer" onClick={()=> setTheme(1)}>
@@ -52,15 +55,37 @@ export default function Home() {
                     <span>테마별</span>
                 </div>
             </div>
+            <div 
+                className={`
+                    w-[calc(100%-20px)] max-w-[1000px] flex justify-center items-center 
+                    mt-[10px] text-[24px] p-[8px] rounded-[8px]
+                    border-[3px] border-solid border-[#7bed9f]
+                `}
+            >
+                <div 
+                    className={`
+                        bg-[#7bed9f] w-[calc(50%-8px)] h-[40px] rounded-[4px] 
+                        absolute transition-all left-0 translate-x-[100%] font-bold
+                        ${ui === true && '!translate-x-[8px]'}
+                        ${ui === false && '!translate-x-[calc(100%+8px)]'}
+                    `}
+                />
+                <div className="w-full flex justify-center items-center h-[40px] cursor-pointer" onClick={()=> setUi(true)}>
+                    <p className="flex justify-center items-center font-bold">카드UI <PiCardsBold /></p>
+                </div>
+                <div className="w-full flex justify-center items-center h-[40px] cursor-pointer" onClick={()=> setUi(false)}>
+                    <p className="flex justify-center items-center font-bold">리스트UI <PiListFill /></p>
+                </div>
+            </div>
 
             {
                 theme === 0 && LoadData.list[0].split(',').filter((title:string) => title.includes(keyword)).map((gameTitle:string, index:number)=>(
                     <div key={index} className="w-[calc(100%-20px)] max-w-[1000px] mb-[80px]">
                         <h4 className="text-[22px] font-semibold mt-[40px] mb-[10.5px]">{gameTitle}</h4>
-                        <div className="w-full flex flex-wrap justify-start items-center gap-[14px]">
+                        <div className={`w-full flex flex-wrap justify-start items-center gap-[14px] ${ui === false && 'flex-col'}`}>
                             {
                                 LoadData.data.filter((item:prop)=> item.title !== "" && gameTitle === item.game_ko).map((content, index) => (
-                                    <ContentBox key={index} content={content}/>
+                                    <ContentBox ui={ui} key={index} content={content}/>
                                 ))
                             }
                         </div>
@@ -69,13 +94,17 @@ export default function Home() {
             }
             {
                 <div className={`w-[calc(100%-20px)] max-w-[1000px] mb-[80px] ${theme !== 1 && '!hidden'}`}>
-                    <div className={`w-full flex flex-wrap justify-start items-center gap-[14px] mt-[40px]`}>
+                    <div className={`w-full flex flex-wrap justify-start items-center gap-[14px] mt-[40px]  ${ui === false && 'flex-col'}`}>
                         {
                             LoadData.data.filter((item:prop)=> item.title !== "" && item.title.includes(keyword)).sort((a:prop, b:prop) => a.title.localeCompare(b.title, 'ko')).map((content:prop, index:number) => (
-                                <ContentBox key={index} content={content}/>
+                                <ContentBox ui={ui} key={index} content={content}/>
                             ))
                         }
                     </div>
+                </div>
+            }
+            {
+                <div className={`w-[calc(100%-20px)] max-w-[1000px] min-h-screen mb-[80px] ${theme !== 2 && '!hidden'}`}>
                 </div>
             }
         </div>
